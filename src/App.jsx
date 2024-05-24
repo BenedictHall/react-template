0f968aed05288f58a7e8ee3e661d45d8c10e3178
Benedict <BenedictH2468@gmail.com>
2024-05-24 13:19:38 +0100

    added profile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hello from "./components/Hello";
 import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
+import Profile from "./profile.jsx";
 
 function App() {
   return (
     <>
       <Hello name="World" />
       <img className="logo" src={makersLogo}></img>
+      <Profile />    
     </>
   );
 }
